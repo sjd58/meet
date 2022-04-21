c5814d8a29d460b5968de8c3896647327e1a3896
sjd58 <dicksonsj0798@gmail.com>
2022-04-21 15:03:08 -0400

    Update NumberOfEvents.js

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -14,13 +14,15 @@ class NumberOfEvents extends Component {
 
   render() {
     return(
-      <div className="NumberOfEvents">
-        <input
-          className="number-of-events"
-          type="number"
-          value={this.props.numberOfEvents}
-          onChange={this.handleInputChange}
-        ></input>
+      <div className="NumberOfEvents">Number of Events:
+        <div>
+          <input
+            className="number-of-events"
+            type="number"
+            value={this.props.numberOfEvents}
+            onChange={this.handleInputChange}
+          ></input>
+        </div>
       </div>
     );
   }
