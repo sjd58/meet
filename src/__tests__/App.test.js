877a69fe41d2c46cf3ba6336c2a8dce6f1ce065b
sjd58 <dicksonsj0798@gmail.com>
2022-04-27 12:36:10 -0400

    Corrected class selector on test

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -60,7 +60,7 @@ describe('<App /> integration', () => {
   
   test('get list of all events when user selects "see all cities"', async () => {
     const AppWrapper = mount(<App />);
-    const suggestionItems = AppWrapper.find(CitySearch).find('suggestions li');
+    const suggestionItems = AppWrapper.find(CitySearch).find('.suggestions li');
     await suggestionItems.at(suggestionItems.length - 1).simulate('click');
     const allEvents = await getEvents();
     expect(AppWrapper.state('events')).toEqual(allEvents);
