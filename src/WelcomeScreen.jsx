9a65f7efe6ea327b8c009e2788cffd5cb78733cb
sjd58 <dicksonsj0798@gmail.com>
2022-05-03 22:31:00 -0400

    Create WelcomeScreen.jsx

---
+++ src/WelcomeScreen.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './WelcomeScreen.css';
+
+function WelcomeScreen(props) {
+  return props.showWelcomeScreen ? 
+    (
+
+    )
+  : null
+}
+
+export default WelcomeScreen
