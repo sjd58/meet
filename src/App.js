be9ccfc628f8158291e767fcf0e239bc4ab9f7ab
sjd58 <dicksonsj0798@gmail.com>
2022-05-09 16:15:19 -0400

    Add initial version of a scatter plot

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import CitySearch from './CitySearch';
 import NumberOfEvents from './NumberOfEvents';
 import { extractLocations, getEvents } from './api';
 import './nprogress.css';
+import { ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
 
 class App extends Component {
   state = {
@@ -56,17 +57,48 @@ class App extends Component {
     }
   };
 
+  getData = () => {
+    const {locations, events} = this.state;
+    const data = locations.map((location)=>{
+      const number = events.filter((event) => event.location === location).length
+      const city = location.split(', ').shift()
+      return {city, number};
+    })
+    return data;
+  };
+
   render() {
+    const { locations, numberOfEvents, events } = this.state;
     return (
       <div className ="App">
+        <h1>Meet App</h1>
+        <h4>Choose your nearest city!</h4>
+
         <CitySearch 
           locations={this.state.locations}
           updateEvents={this.updateEvents}
         />
+
         <NumberOfEvents
           numberOfEvents={this.state.numberOfEvents}
           updateEvents={this.updateEvents} 
         />
+
+        <h4>Events in each city</h4>
+        <ResponsiveContainer height={400} >
+          <ScatterChart
+            margin={{
+              top: 20, right: 20, bottom: 20, left: 20,
+            }}
+          >
+            <CartesianGrid />
+            <XAxis type="category" dataKey="city" name="city" />
+            <YAxis allowDecimals={false} type="number" dataKey="number" name="number of events" />
+            <Tooltip cursor={{ strokeDasharray: "3 3"}} />
+            <Scatter data={this.getData()} fill="#8884d8" />
+          </ScatterChart>
+        </ResponsiveContainer>
+
         <EventList 
           events={this.state.events} 
         />
