5e898a67477741608393244bf5ab50247de3a357
sjd58 <dicksonsj0798@gmail.com>
2022-05-13 11:10:57 -0400

    Update EventGenre.js

--- src/EventGenre.js
+++ src/EventGenre.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { PieChart, Pie, Cell, ResponsiveContainer } from 'recharts';
+import { PieChart, Pie, ResponsiveContainer } from 'recharts';
 
 const EventGenre = ({ events }) => {
   const [data, setData] = useState([]);
